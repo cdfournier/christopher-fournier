c8b9700dfe82bb9c8c9b41519a80c017da3b07d5
Chris <chris.d.fournier@gmail.com>
2020-04-21 12:38:28 -0400

    Added aria details to projects menu
    Added aria-expanded and aria-hidden tags to appropriate navigation items and updated site script to modify values based on user interaction; fixed class tag issues on error pages; also fixed hover and focus issue on project menu close button (li hover was dimming the link to 0.5.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,10 @@
 $('.open--projects').click(function() {
   $('.projects-list').addClass('show');
+  $('.open--projects').attr('aria-expanded',true);
+  $('aside nav ul').attr('aria-hidden',false);
 });
 $('.close--projects').click(function() {
   $('.projects-list').removeClass('show');
+  $('.open--projects').attr('aria-expanded',false);
+  $('aside nav ul').attr('aria-hidden',true);
 });
